6d989d1a9b130a260c1ffd5761f4be6ce812ebe0
pallavee <pallaveeratnakar1714@gmail.com>
2022-12-15 19:04:06 +0530

    updation_in_projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
           git="https://github.com/Mohammadzaki0027/hurried-hospital-4011"
           tech={["HTML", "CSS", "Javascript", "React","Redux","MongoDB","Express","Chakra UI","GitHub","Heroku"]}
           desc="The Website is use to book the Flight, Cars, Hotels, at reasonable price and it is vary on customer expectation."
-          pFName="WAY_TO_GLOBE"
+          pFName="WayToGlobe"
           pLName="Clone"
           image={waytogloble}
         />
