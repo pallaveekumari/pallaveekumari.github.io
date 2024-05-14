65e1f901597bfd5256c46a67c25e175a2101847f
pallaveekumari <pallaveeratnakar1714@gmail.com>
2024-05-14 14:40:03 +0530

    resume-update

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -33,7 +33,7 @@ const Intro = () => {
         
         <a href={Resume} download>
           <button onClick={()=>{
-            window.open("https://drive.google.com/file/d/1tNX3RQO6Xb5xrFJ_TZ1wCTh_Z2aT157T/view?usp=sharing","_blank")
+            window.open("https://drive.google.com/file/d/1oksAzz9dgu7gs7aLprqChb7Q9GabYWCH/view?usp=sharing","_blank")
           }}className="button s-button">Download Resume</button>
         </a>
         <div className="i-icons">
